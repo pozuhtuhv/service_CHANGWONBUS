5bd1b4f94a1378e50f9fdd647c3e05f4dfc29947
Hyeonseok-Cho <pozuhtuhv@gmail.com>
2024-08-29 17:39:06 +0900

    (edit) js_setting

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,28 @@
-const SERVICE_KEY = process.argv[2];
+const SERVICE_KEY = '...'
 
-if (!SERVICE_KEY) {
-  console.error("SERVICE_KEY is not provided");
-  process.exit(1);
-}
-
-// SERVICE_KEY를 사용하는 로직
-console.log("Service Key:", SERVICE_KEY);
-
-// 창원시 운용중인 전체 [버스] 데이터를 가져오는 함수
+// 창원시 운용중인 전체 [버스] 데이터를 가져오는 함수 [1-1]
 async function fetchBusData() {
     const response = await fetch('https://pozuhtuhv.github.io/service_CHANGWONBUS/data/%5B1-1%5Dbusdata.json');
     const data = await response.json();
     return data.ServiceResult.MsgBody.BusList.row;
 }
 
-// 창원시 운용중인 전체 [정류장] 데이터를 가져오는 함수
+// 창원시 운용중인 전체 [정류소] 데이터를 가져오는 함수 [1-3]
 async function fetchStationData() {
     const response = await fetch('https://pozuhtuhv.github.io/service_CHANGWONBUS/data/%5B1-3%5Dstationdata.json');
     const data = await response.json();
     return data.ServiceResult.MsgBody.StationList.row;
+}
+
+// TEST
+// 창원시 해당버스 전체 [정류소]를 가져오는 함수 [3-1]
+async function fetchLocation(ROUTE_ID) {
+    const response = await fetch(`http://openapi.changwon.go.kr/rest/bis/BusLocation/?serviceKey=${SERVICE_KEY}&route=${ROUTE_ID}`);
+}
+
+// 창원시 버스 위치 데이터를 가져오는 함수 [4-1]
+async function fetchLocation(ROUTE_ID) {
+    const response = await fetch(`http://openapi.changwon.go.kr/rest/bis/BusPosition/?serviceKey=${SERVICE_KEY}&route=${ROUTE_ID}`);
 }
 
 // 정류장 ID를 정류장 이름으로 변환하는 함수
@@ -27,8 +30,9 @@ function getStationName(stationId, stationData) {
     const station = stationData.find(station => station.STATION_ID === stationId);
     return station ? station.STATION_NM : '알 수 없음';
 }
+// TEST
 
-// JSON 데이터 필드를 좀 더 이해하기 쉬운 형태로 변환하는 함수
+// JSON 데이터 변환 정제
 async function transformBusInfo(busInfo) {
     const stationData = await fetchStationData();
     const originStationName = getStationName(busInfo.ORGT_STATION_ID, stationData);
@@ -52,7 +56,7 @@ function searchBus(busNum, busData) {
     return busData.filter(row => row.ROUTE_NM.includes(busNum));
 }
 
-// LEFT Section
+// LEFT Section (버스의 기본정보 표시)
 // 검색 결과를 드롭다운으로 표시하는 함수 (왼쪽 섹션)
 function displayResultsLeft(results) {
     const dropdown = document.getElementById('busDropdown1');
@@ -95,7 +99,7 @@ async function handleSubmitLeft(event) {
     displayResultsLeft(matchingBuses);
 }
 
-// RIGHT Section
+// RIGHT Section (버스의 현재 위치와 다음 정류장 표시)
 // 검색 결과를 드롭다운으로 표시하는 함수 (오른쪽 섹션)
 function displayResultsRight(results) {
     const dropdown = document.getElementById('busDropdown2');
@@ -137,3 +141,8 @@ async function handleSubmitRight(event) {
     const matchingBuses = searchBus(busNum, busData);
     displayResultsRight(matchingBuses);
 }
+
+
+
+
+
